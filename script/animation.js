c99fe14f538f04fc62d19609153988e482fe3216
starskin <95673013+starskin@users.noreply.github.com>
2022-05-21 01:59:53 -0400

    Template start

---
+++ script/animation.js
@@ -0,0 +1,49 @@
+let rocketAnimationElement = document.getElementById("rocket-animation");
+let templateFrame = [
+  "---------------",
+  "-------^-------",
+  "------/ \\------",
+  "-----/   \\-----",
+  "-----|   |-----",
+  "-----|   |-----",
+  "-----|   |-----",
+  "-----|   |-----",
+  "----/|   |\\----",
+  "---/_|   |_\\---",
+  "-----\\___/-----",
+  "------212------",
+  "------*3*------",
+  "-------4-------",
+  "---------------"
+].join("\n")
+var step = 0;
+var exhaustChars = ['@','#','$','%','&'];
+let charNum = exhaustChars.length;
+setInterval(animateRocket,100)
+
+function getExhaustChars(stepNum){
+  let chars = [];
+  for (let i = 0; i < charNum; i++) {
+    let index = stepNum - i;
+    if (index < 0){
+      index = index + charNum;
+    }
+    chars[i] = exhaustChars[index];
+  }
+  return chars;
+}
+
+function animateRocket(){
+  let rocketFrame = templateFrame;
+  let chars = getExhaustChars(step);
+  rocketFrame = rocketFrame.replace(/1/g, chars[0]);
+  rocketFrame = rocketFrame.replace(/2/g, chars[1]);
+  rocketFrame = rocketFrame.replace(/3/g, chars[2]);
+  rocketFrame = rocketFrame.replace(/4/g, chars[3]);
+  rocketAnimationElement.innerHTML = rocketFrame;
+  if (step === 4){
+    step = 0;
+  } else{
+    step = step + 1;
+  }
+}
