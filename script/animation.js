0582fa7a13d0679d699b275def8ef577e22f5b8c
starskin <95673013+starskin@users.noreply.github.com>
2022-05-22 20:49:46 -0400

    Updated appearance

--- script/animation.js
+++ script/animation.js
@@ -1,6 +1,6 @@
 let rocketAnimationElement = document.getElementById("rocket-animation");
 let templateFrame = [
-  "---------------",
+  " ------------- ",
   "-------^-------",
   "------/ \\------",
   "-----/   \\-----",
@@ -14,9 +14,10 @@ let templateFrame = [
   "------212------",
   "------*3*------",
   "-------4-------",
-  "---------------"
+  " ------------- "
 ].join("\n")
 var step = 0;
+let templateLength = templateFrame.length;
 var exhaustChars = ['@','#','$','%','&'];
 let charNum = exhaustChars.length;
 setInterval(animateRocket,100)
@@ -36,12 +37,16 @@ function getExhaustChars(stepNum){
 function animateRocket(){
   let rocketFrame = templateFrame;
   let chars = getExhaustChars(step);
+  let index = Math.floor(Math.random() * templateLength);
   rocketFrame = rocketFrame.replace(/1/g, chars[0]);
   rocketFrame = rocketFrame.replace(/2/g, chars[1]);
   rocketFrame = rocketFrame.replace(/3/g, chars[2]);
   rocketFrame = rocketFrame.replace(/4/g, chars[3]);
+  if (rocketFrame[index] == "-" && step == 4){
+    rocketFrame = rocketFrame.slice(0,index) + "+" + rocketFrame.slice(index + 1,templateLength);
+  }
   rocketAnimationElement.innerHTML = rocketFrame;
-  if (step === 4){
+  if (step == 4){
     step = 0;
   } else{
     step = step + 1;
